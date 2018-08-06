1fb448684d02647a72b46676c043870ba5f5ea49
Jake <jake@jaketollette.com>
2018-08-06 18:39:51 -0500

    Update projects to use core 2.1

---
+++ CoreApp.Infrastructure/Migrations/20180806233743_initial.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreApp.Infrastructure.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deleted = table.Column<bool>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Icons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deleted = table.Column<bool>(nullable: true),
+                    FileName = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Icons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Leaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deleted = table.Column<bool>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Active = table.Column<bool>(nullable: true),
+                    DenId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leaders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Leaders_Dens_DenId",
+                        column: x => x.DenId,
+                        principalTable: "Dens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Badges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deleted = table.Column<bool>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Summary = table.Column<string>(nullable: true),
+                    IconId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Badges", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Badges_Icons_IconId",
+                        column: x => x.IconId,
+                        principalTable: "Icons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ranks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deleted = table.Column<bool>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Order = table.Column<int>(nullable: false),
+                    IconId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ranks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ranks_Icons_IconId",
+                        column: x => x.IconId,
+                        principalTable: "Icons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deleted = table.Column<bool>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Birthday = table.Column<DateTime>(nullable: true),
+                    RankId = table.Column<int>(nullable: false),
+                    DenId = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: true),
+                    Active = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scouts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Scouts_Dens_DenId",
+                        column: x => x.DenId,
+                        principalTable: "Dens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Scouts_Ranks_RankId",
+                        column: x => x.RankId,
+                        principalTable: "Ranks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScoutBadges",
+                columns: table => new
+                {
+                    ScoutId = table.Column<int>(nullable: false),
+                    BadgeId = table.Column<int>(nullable: false),
+                    IconId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScoutBadges", x => new { x.BadgeId, x.ScoutId });
+                    table.ForeignKey(
+                        name: "FK_ScoutBadges_Badges_BadgeId",
+                        column: x => x.BadgeId,
+                        principalTable: "Badges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScoutBadges_Icons_IconId",
+                        column: x => x.IconId,
+                        principalTable: "Icons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ScoutBadges_Scouts_ScoutId",
+                        column: x => x.ScoutId,
+                        principalTable: "Scouts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Badges_IconId",
+                table: "Badges",
+                column: "IconId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leaders_DenId",
+                table: "Leaders",
+                column: "DenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ranks_IconId",
+                table: "Ranks",
+                column: "IconId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScoutBadges_IconId",
+                table: "ScoutBadges",
+                column: "IconId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScoutBadges_ScoutId",
+                table: "ScoutBadges",
+                column: "ScoutId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scouts_DenId",
+                table: "Scouts",
+                column: "DenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scouts_RankId",
+                table: "Scouts",
+                column: "RankId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leaders");
+
+            migrationBuilder.DropTable(
+                name: "ScoutBadges");
+
+            migrationBuilder.DropTable(
+                name: "Badges");
+
+            migrationBuilder.DropTable(
+                name: "Scouts");
+
+            migrationBuilder.DropTable(
+                name: "Dens");
+
+            migrationBuilder.DropTable(
+                name: "Ranks");
+
+            migrationBuilder.DropTable(
+                name: "Icons");
+        }
+    }
+}
