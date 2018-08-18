4fca37523e9e4afe88cc9346c9de0dc8fbade0d0
Jake <jake@jaketollette.com>
2018-08-18 13:34:37 -0500

    Update to use Angular frontend

--- CoreApp.WebUI/Startup.cs
+++ CoreApp.WebUI/Startup.cs
@@ -1,72 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using CoreApp.Core.Abstract;
 using CoreApp.Core.Services;
 using CoreApp.Infrastructure.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreApp.WebUI
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddAutoMapper();
-            services.AddMvc();
-
-            // Setup database in virtual method for test override.
-            ConfigureDatabase(services);
-
-            services.AddTransient<IScoutService, ScoutService>();
-            services.AddTransient<IScoutRepository, ScoutRepository>();
-        }
-
-        public virtual void ConfigureDatabase(IServiceCollection services)
-        {
-            services.AddDbContext<ScoutContext>(opts =>
-            {
-                opts.UseSqlServer(Configuration.GetConnectionString("ScoutDatabase"))
-                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // Marked virtual for test override.
-        public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAutoMapper();
+      services.AddMvc();
+
+      // Setup database in virtual method for test override.
+      ConfigureDatabase(services);
+
+      services.AddTransient<IScoutService, ScoutService>();
+      services.AddTransient<IScoutRepository, ScoutRepository>();
+    }
+
+    public virtual void ConfigureDatabase(IServiceCollection services)
+    {
+      services.AddDbContext<ScoutContext>(opts =>
+      {
+        opts.UseSqlServer(Configuration.GetConnectionString("ScoutDatabase"))
+              .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    // Marked virtual for test override.
+    public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseBrowserLink();
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseSpa(spa =>
+      {
+        spa.Options.SourcePath = "src";
+        if (env.IsDevelopment())
+        {
+          spa.UseAngularCliServer(npmScript: "start");
+        }
+      });
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+
+        routes.MapSpaFallbackRoute(
+            name: "spa-fallback",
+            defaults: new { controller = "Home", action = "Index" });
+      });
+    }
+  }
 }
